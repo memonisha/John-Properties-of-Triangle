dac30165a6c0e4e46047135781ff633003642a17
memonisha <75033521+memonisha@users.noreply.github.com>
2024-03-09 08:02:57 +0530

    Add files via upload

--- sketch.py
+++ sketch.py
@@ -1,4 +1,5 @@
 from p5 import *
+import math  #library in python to do math stuff like sqrt, round
 
 
 def setup():
@@ -40,3 +41,42 @@ def draw():
   stroke(51, 245, 61)
   line(c.x,c.y, a.x, a.y)
   pop()
+
+  distance()
+
+#distances
+#p1=(20,30)    p2=(100,230)
+# return ----> p1 and p2 distance
+#    formula= sqrt()
+def distance():
+  
+  ab= round(math.sqrt((a.x-b.x)**2+(a.y-b.y)**2))
+  bc= round(math.sqrt((b.x-c.x)**2+(b.y-c.y)**2))
+  ac= round(math.sqrt((a.x-c.x)**2+(a.y-c.y)**2))
+
+  midXab=(a.x+b.x)/2 - 25
+  midYab=(a.y+b.y)/2
+  textSize(13)
+  fill("plum")
+  textStyle(BOLD)
+  text(ab,midXab,midYab)
+  midXbc =(b.x+c.x)/2 +15
+  midYbc =(b.y+c.y)/2 
+  text(bc,midXbc,midYbc)
+  midXac =(a.x+c.x)/2 -10
+  midYac =(a.y+c.y)/2 -15
+  text(ac,midXac,midYac)
+  textSize (20)
+  if ab==bc==ac :
+    
+    text("This triangle is Equilateral",width/6,height -100)
+
+  elif ab==bc or bc==ac or ab==ac :
+    
+    text("This triangle is Isosceles",width/6,height -100)
+
+  else:
+    
+    text("This triangle is Scalene",width/6,height -100)
+  
+  print(ab,bc,ac)
